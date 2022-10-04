fe71102b26c0613b295a40bc4260134ac0c63562
IzzyAIR <izzycode2105@gmail.com>
2022-10-04 23:34:38 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import './index.scss';
+
+
+const Modal = ({open, setOpen, children}) => (
+  <div className={`overlay animated ${open ? 'show': ''}`}>
+      <div className="modal">
+        <svg height="200" viewBox="0 0 200 200" width="200" onClick={()=> setOpen(false)}>
+          <title />
+          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+        </svg>
+        {children}
+      </div>
+  </div>
+)
+
+
+
+function App() {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <div className="App">
+      <button onClick={()=> setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
+      <Modal open={open} setOpen={setOpen}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='Gifpy' />
+        <h3>это модаль окно</h3>
+      </Modal>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /* Анимация без компонента  */
+
+      /* {<div className={`overlay animated ${open ? 'show': ''}`}>
+        <div className="modal">
+          <svg height="200" viewBox="0 0 200 200" width="200" onClick={()=> setOpen(false)}>
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        </div>
+      </div>} */
+
+
+
+
+     /*Условное без анимации */
+
+      /* {open && ( <div className='overlay' >
+        <div className="modal">
+          <svg height="200" viewBox="0 0 200 200" width="200" onClick={()=> setOpen(false)}>
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        </div>
+      </div>)} */
